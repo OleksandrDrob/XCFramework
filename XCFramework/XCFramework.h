6b6c776e5acc121753af12b9dbedffbf381ce359
OleksandrDrob <oleksandr.drobyshev@jadexconsulting.com>
2025-01-08 10:58:37 +0200

    Initial Commit

---
+++ XCFramework/XCFramework.h
@@ -0,0 +1,18 @@
+//
+//  XCFramework.h
+//  XCFramework
+//
+//  Created by Oleksandr Drobyshev on 08.01.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XCFramework.
+FOUNDATION_EXPORT double XCFrameworkVersionNumber;
+
+//! Project version string for XCFramework.
+FOUNDATION_EXPORT const unsigned char XCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XCFramework/PublicHeader.h>
+
+
